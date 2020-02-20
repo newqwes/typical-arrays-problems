b47650766a1a9bb236fe9c6aeae920cf6aac386a
Nazar Ashirov <newnewqwes@gmail.com>
2020-02-20 15:35:18 +0300

    init: Done (Typical Arrays Problems)

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array == undefined) return 0;
+  if (array.length > 0) return Math.min(...array) // spred operator
+  else return 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array == undefined) return 0;
+  if (array.length > 0) return Math.max(...array)
+  else return 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array == undefined) return 0;
+  if (array.length > 0) return array.reduce((acc, item) => acc + item) / array.length; //reduce (accumulator, currentValue) => accumulator + currentValue;
+  else return 0;
 }
